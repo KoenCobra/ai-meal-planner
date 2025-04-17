204283bbd62a9dcb72519b1996f578cbe364b25a
Koen De Groot <koen.degroot@organimmo.be>
2025-04-17 18:16:07 +0200

    Integrate SchematicComponent into ManagePlan page for enhanced functionality

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -1,7 +1,14 @@
+import SchematicComponent from "@/components/schematic/SchematicComponent";
 import React from "react";
 
 const ManagePlan = () => {
-  return <div>ManagePlan</div>;
+  return (
+    <div>
+      <SchematicComponent
+        componentId={process.env.NEXT_PUBLIC_SCHEMATIC_COMPONENT_ID!}
+      />
+    </div>
+  );
 };
 
 export default ManagePlan;
