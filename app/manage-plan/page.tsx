db9902239ff966f10da25b460f0b1769f4c2383c
Koen De Groot <koen.degroot@organimmo.be>
2025-04-17 18:25:53 +0200

    Enhance ManagePlan page with improved layout and descriptive content

--- app/manage-plan/page.tsx
+++ app/manage-plan/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 
 const ManagePlan = () => {
   return (
-    <div>
+    <div className="xl:max-w-5xl mx-auto p-4 md:p-0">
+      <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
+      <p className="text-gray-600 mb-8">
+        Manage your plan and billing information.
+      </p>
       <SchematicComponent
         componentId={process.env.NEXT_PUBLIC_SCHEMATIC_COMPONENT_ID!}
       />
