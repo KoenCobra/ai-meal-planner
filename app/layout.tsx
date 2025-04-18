4cf5ec1ea26a150e9664bbff4433ba74a3d29f86
Koen De Groot <koen.degroot@organimmo.be>
2025-04-18 17:17:29 +0200

    Add theme support by integrating next-themes and ThemeToggle component; update package.json with new dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,13 +29,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider dynamic>
           <ConvexClientProvider>
-            <main>{children}</main>
+            <main>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                {children}
+              </ThemeProvider>
+            </main>
           </ConvexClientProvider>
         </ClerkProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import ThemeToggle from "@/components/ThemeToggle";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  );
 };
 
 export default Home;
