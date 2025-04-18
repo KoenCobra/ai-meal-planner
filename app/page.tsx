35fb88b4f6167e50ea1e9a68434992998b803355
Koen De Groot <koen.degroot@organimmo.be>
2025-04-18 17:20:00 +0200

    Add authentication buttons to Home component using Clerk; display SignIn and SignOut options based on user status

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import ThemeToggle from "@/components/ThemeToggle";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <ThemeToggle />
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   );
 };
