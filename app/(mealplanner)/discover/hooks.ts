2bdb8e4592dfeca265474105eeec7ccc2049eed7
Koen De Groot <koendegroot84@gmail.com>
2025-04-21 20:30:29 +0200

    Refactor useRecipe hook to remove user authentication logic, simplifying the code and improving maintainability.

--- app/(mealplanner)/discover/hooks.ts
+++ app/(mealplanner)/discover/hooks.ts
@@ -1,17 +1,8 @@
-"use server";
-
 import { RecipeDetail, DiscoverRecipeItem } from "@/app/types/recipe";
 import { axiosInstance } from "@/app/utils/axiosInstance";
-import { useAuth } from "@clerk/nextjs";
 import { useQuery } from "@tanstack/react-query";
 
 export const useRecipe = (id: string) => {
-  const { userId } = useAuth();
-
-  if (!userId) {
-    throw new Error("User not found in useRecipe hook");
-  }
-
   const { data, isError, isLoading } = useQuery<RecipeDetail>({
     queryKey: ["recipe", id],
     queryFn: async () => {
