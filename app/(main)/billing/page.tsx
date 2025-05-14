9631b1a3f9c9b7733d63c8869e6f18a2451594db
Koen De Groot <koendegroot84@gmail.com>
2025-05-14 18:26:59 +0200

    Implement user plan validation in meal planner layout to redirect unauthenticated users to billing page. Utilize `auth()` helper to check for specific plans before rendering the layout.

---
+++ app/(main)/billing/page.tsx
@@ -0,0 +1,11 @@
+import { PricingTable } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div className="container mx-auto ">
+      <PricingTable />
+    </div>
+  );
+};
+
+export default page;
--- app/(mealplanner)/layout.tsx
+++ app/(mealplanner)/layout.tsx
@@ -1,8 +1,22 @@
 import ErrorBoundary from "@/components/ui/ErrorBoundary";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 import Navbar from "./_components/Navbar";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
+  // Use `auth()` helper to access the `has()` method
+  const { has } = await auth();
+
+  // Use `has()` method to check if user has a Plan
+  const hasMasterMealLordPlan = has({ plan: "master_meal_lord" });
+  const hasSeriousSizzlerPlan = has({ plan: "serious_sizzler" });
+  const hasCuriousCookPlan = has({ plan: "curious_cook" });
+
+  if (!hasMasterMealLordPlan && !hasSeriousSizzlerPlan && !hasCuriousCookPlan) {
+    redirect("/billing");
+  }
+
   return (
     <div>
       <Navbar />
