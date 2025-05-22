f5aa14d9ea862e93c728e87843a0b4b0ea2ad685
Koen De Groot <koendegroot84@gmail.com>
2025-05-22 08:26:35 +0200

    Add react-scan dependency and update layout for potential integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* <ReactScan /> */}
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-svh`}
