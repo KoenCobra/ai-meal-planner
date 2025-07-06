8655ca5987e1ad6e05dfab05b044545a850772a2
Koen De Groot <koendegroot84@gmail.com>
2025-07-06 20:06:51 +0200

    Add unoptimized attribute to image in BubuAiForm for performance improvement

--- app/(mealplanner)/bubu-ai/_components/BubuAiForm.tsx
+++ app/(mealplanner)/bubu-ai/_components/BubuAiForm.tsx
@@ -184,6 +184,7 @@ const BibiAiForm = ({
                       fill
                       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                       className="object-cover"
+                      unoptimized
                     />
                   </div>
                   <Button
